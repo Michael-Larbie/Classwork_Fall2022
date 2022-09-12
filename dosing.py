abe9576d3c13fd133fc00fb6fb0d5b60828c7864
Michael Larbie <64946798+Michael-Larbie@users.noreply.github.com>
2022-09-12 13:04:24 -0400

    Created more functions in the code to make it more modular

--- dosing.py
+++ dosing.py
@@ -1,4 +1,4 @@
- """dosing.py
+  """dosing.py
     Example program of calculating first-day dose of medicine for pediatric
         patients.
     NOTE:  This is a programming example, and should not be used for any
@@ -6,7 +6,7 @@
 """
 
 
-def dose_amount():
+def determine_diagnosis():
     print("Day One Dosing Guidelines")
     print("")
     print("Choose diagnosis:")
@@ -15,6 +15,9 @@ def dose_amount():
     print("3 - Community-acquired pneumonia")
     print("4 - Pharyngitis/tonsilitis")
     diagnosis = int(input("Enter a number: "))
+    return diagnosis
+    
+def weight_finder():
     print("PATIENT WEIGHT")
     print("Enter patient weight followed by units of kg or lb.")
     print("Examples:  65.3 lb      21.0 kg")
@@ -22,6 +25,9 @@ def dose_amount():
     weight_data = weight_input.split(" ")
     weight = float(weight_data[0])
     units = weight_data[1]
+    return weight, units
+    
+def dosage_amount(diagnosis,weight,units):
     if units == "lb":
         weight = weight / 2.205
     dosages_mg_per_kg = [30, 10, 10, 12]
@@ -34,4 +40,7 @@ def dose_amount():
 
 
 if __name__ == '__main__':
-    dose_amount()
+    determine_diagnosis()
+    weight_finder()
+    dosage_amount(diagnosis,weight,units)
+    
