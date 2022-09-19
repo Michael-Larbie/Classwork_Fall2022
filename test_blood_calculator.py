c296a8b3b14781764b4c95d860eaeb3456129ce0
Michael Larbie <64946798+Michael-Larbie@users.noreply.github.com>
2022-09-19 13:06:09 -0400

    Added tests for check ldl and cholesterol

--- test_blood_calculator.py
+++ test_blood_calculator.py
@@ -1,15 +1,34 @@
 import pytest
 
-@pytest.mark.parametrize("input_one,input_two", 
+@pytest.mark.parametrize("input_one,expected", 
     [(85,"Normal"),
      (45,"Borderline low"),
      (20, "Low")])
 
-def test_check_HDL_Normal(input_one, input_two):
+def test_check_HDL_Normal(input_one, expected):
     from blood_calculator import check_HDL
     answer = check_HDL(input_one)
-    assert answer == input_two
+    assert answer == expected
     
+@pytest.mark.parametrize("input_one, expected",
+    [(120,"Normal"),
+     (140,"Borderline high"),
+     (170,"High")])
+
+def test_check_LDL_Normal(input_one, expected):
+    from blood_calculator import check_LDL
+    answer = check_LDL(input_one)
+    assert answer == expected
+    
+@pytest.mark.parametrize("input_one, expected",
+    [(150,"Normal"),
+     (220,"Borderline high"),
+     (260,"High")])
+
+def test_check_Cholesterol_Normal(input_one, expected):
+    from blood_calculator import check_Cholesterol
+    answer = check_Cholesterol(input_one)
+    assert answer == expected  
 '''   
 def test_check_HDL_BorderlineLow():
     from blood_calculator import check_HDL
