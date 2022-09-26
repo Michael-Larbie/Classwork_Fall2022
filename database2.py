e07538b4267cde36312a0b324cd3780bd6a3bda4
Michael Larbie <64946798+Michael-Larbie@users.noreply.github.com>
2022-09-26 13:55:27 -0400

    Made changes and added fucntions to account for the use of a dictionary

--- database2.py
+++ database2.py
@@ -1,3 +1,16 @@
+class Patient:
+    def __init__(self):
+        self.first_name = ''
+        self.last_name = ''
+        self.patient_id = ''
+        self.age = ''
+        self.tests = []
+        x = 5
+
+    def full_name(self):
+        return "{} {}".format(self.first_name, self.last_name)
+
+
 def create_patient_entry(patient_first_name, patient_last_name, patient_id,
                          patient_age):
     new_patient = {"first name": patient_first_name,
@@ -9,48 +22,20 @@ def create_patient_entry(patient_first_name, patient_last_name, patient_id,
 
 
 def main():
-    db = []
-    db.append(create_patient_entry("Ann", "Ables", 1, 30))
-    db.append(create_patient_entry("Bob", "Boyles", 2, 34))
-    db.append(create_patient_entry("Chris", "Chou", 3, 25))
-    print(finder(db,2))
+    # x = Patient()
+    # x.first_name = 'David'
+    # x.last_name = "Ward"
+    # print(x.last_name)
+    # y = Patient()
+    # y.first_name = 'Edward'
+    # y.last_name = 'Smith'
+    # print(y.last_name)
+    # exit()
+    db = {}
+    db[1] = create_patient_entry("Ann", "Ables", 1, 30)
+    db[2] = create_patient_entry("Bob", "Boyles", 2, 34)
+    db[3] = create_patient_entry("Chris", "Chou", 3, 25)
+    print(finder(db, 2))
     print(test(db, 3, "HDL", 100))
     print("Patient {} is a {}".format(get_full_name(db[2]),
-                                      adult_or_minor(db[2])))
-
-
-def directory(db):
-    for i in db:
-        print(i)
-        print("Name: {}, Id: {}, Age: {}".format(i['first name'],
-                                                 i['id'],
-                                                 i["age"]))
-
-
-def finder(db, id_no):
-    for i in db:
-        if i['id'] == id_no:
-            return i
-    return False
-
-
-def test(db, id_no, tname, tvalue):
-    patient = finder(db, id_no)
-    patient["test"].append((tname, tvalue))
-    return patient
-
-
-def get_full_name(patient):
-    full_name = "{} {}".format(patient["first name"], patient['last name'])
-    return full_name
-
-
-def adult_or_minor(patient):
-    if patient["age"] >= 18:
-        return "adult"
-    else:
-        return "minor"
-
-
-if __name__ == "__main__":
-    main()
+                                      adult_or_minor(db[2])
